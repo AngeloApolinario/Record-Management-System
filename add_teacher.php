a6719f6d83a7c35bc5d540b45c5b7302c79e01cf
Nikus Miguel <niap.miguel.au@phinmaed.com>
2024-10-08 13:45:26 +0800

    Added alot of features

--- add_teacher.php
+++ add_teacher.php
@@ -1,13 +1,17 @@
 <?php
 include 'database_conn.php';
- include("C:/xampp/htdocs/Record-Management-System-second_revision/navbar.php"); 
+include("C:/xampp/htdocs/Record-Management-System-second_revision/navbar.php");
 
 if (isset($_POST['submit'])) {
     $name = $_POST['name'];
     $subject = $_POST['subject'];
+    $gender = $_POST['contact_info'];
     $contact_info = $_POST['contact_info'];
+    $home_address = $_POST['home_address'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-    $query = "INSERT INTO teachers (name, subject, contact_info) VALUES ('$name', '$subject', '$contact_info')";
+    $query = "INSERT INTO teachers (name, subject, contact_info) VALUES ('$name', '$subject',$gender, '$contact_info', '$home_address', '$username', '$password')";
 
     if (mysqli_query($conn, $query)) {
         header("Location: view_teachers.php");
@@ -26,6 +30,31 @@ if (isset($_POST['submit'])) {
     <link rel="stylesheet" href="sidebar-navbar.css">
     <link rel="stylesheet" type="text/css" href="/Record-Management-System-second_revision/sidebar-navbar.css">
 </head>
+<style>
+    label {
+        font-family: Arial, sans-serif;
+        font-size: 16px;
+        color: #333;
+    }
+
+    select {
+        padding: 10px;
+        font-size: 16px;
+        border-radius: 5px;
+        background-color: #f9f9f9;
+        color: #333;
+        width: 150px;
+        cursor: pointer;
+        outline: none;
+    }
+
+    select:focus {
+        border-color: #66afe9;
+        background-color: #fff;
+    }
+
+
+</style>
 
 <body>
 
@@ -45,14 +74,22 @@ if (isset($_POST['submit'])) {
         <a href="#logout" class="logout">Log out</a>
     </div>
 
-  
+
 
     <div class="add_teacher_container">
         <h2>Add New Teacher</h2>
         <form method="POST" action="">
             Name: <input type="text" name="name" required><br>
             Subject: <input type="text" name="subject" required><br>
+            <label for="select_gender">Gender:</label> <br> <br>
+            <select name="gender" id="select_gender">
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+            </select> <br><br>
             Contact Info: <input type="text" name="contact_info" required><br>
+            Home Address: <input type="text" name="home_address" required><br>
+            Username: <input type="text" name="username" required><br>
+            Password: <input type="text" name="password" required><br>
             <button type="submit" name="submit">Add Teacher</button>
         </form>
     </div>
